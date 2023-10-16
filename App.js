b9b814a6b4c4b62dd90435cd851962aa3a7862ab
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-10-16 19:13:18 +0300

    retriivingg

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Current Weather</Text>
+      <Position />
     </View>
   );
 }
@@ -13,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
+
+
+
+
---
+++ components/Weather.js
@@ -0,0 +1,39 @@
+import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+
+const api = {
+    url: 'https://api.openweathermap.org/data/2.5/weather?',
+    key: '9e859dc8498dfe55b8e4cb2be118411d',
+    icons: 'http://openweathermap.org/img/wn/'
+}
+export default function Weather(props) {
+    const [temp, setTemp] = useState(0);
+    const [description, setDescription] = useState('');
+    const [icon, setIcon] = useState('');
+
+    useEffect(() => {
+        const url= api.url + 'lat=' + props.latitude + '&lon=' + props.longitude + '&units=metric&appid=' + api.key;
+
+        fetch(url)  
+            .then( res => res.json())
+            .then((json) => {
+                console.log(json);
+                setTemp(json.main.temp);
+                setDescription(json.weather[0].description);
+                setIcon(api.icons + json.weather[0].icon + '@2x.png');
+            })
+            .catch((error) => {
+                setDescription("errorii")
+                console.log(error);
+            })
+            
+    }, [])
+    return (
+        <View>
+            <Text style={styles.temp}>{temp}</Text>
+            {icon && <Image source={{uri: icon}} style={{width: 100, height: 100}} />
+            }
+            <Text>{description}</Text>
+        </View>
+    )
+}
